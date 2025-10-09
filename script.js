f407a8fa15ca7482eea27acbc9b06a345d8135c5
San Dahan <sandahan007@gmail.com>
2025-10-08 21:04:16 -0400

    Initialize a 3x3 gameboard with cells

--- script.js
+++ script.js
@@ -1,1 +1,30 @@
-console.log('Hello world');
+function Gameboard() {
+    const ROWS = 3;
+    const COLS = 3;
+    const gameboard = [];
+
+    for (let i = 0; i < ROWS; i++) {
+        gameboard[i] = [];
+        for (let j = 0; j < COLS; j++) {
+            gameboard[i].push(Cell());
+        }
+    }
+
+    const printBoard = () => {
+        const board = gameboard.map((row) => row.map((cell) => cell.getValue()));
+        console.log(board);
+    }
+
+    return { printBoard };
+}
+
+function Cell() {
+    let value = '';
+
+    const getValue = () => value;
+
+    return { getValue };
+}
+
+const game = Gameboard();
+game.printBoard();
